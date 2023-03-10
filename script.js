8f1746d13357f82fb8c54df70af178045bc2c607
MuhdNihalCY <nihalcy1234@gmail.com>
2023-03-10 21:16:49 +0530

    modified the Video resources tab

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function LoadMoreVideos() {
     var VideoRow = document.querySelector(".VideoRow");
     var VideoGrid = document.querySelector(".videoGrid");
+    var showLessDiv = document.querySelector(".showLessDiv");
+    var MoreVideo = document.querySelector('.MoreVideo')
     VideoRow.style.display = 'none';
-    VideoGrid.style.display = 'flex';
+    MoreVideo.style.display = 'none';
+    VideoGrid.style.display = 'block';
+    showLessDiv.style.display = 'block';
    // alert("Load More Videos");
 }
 
 function ShowLessVeideo() {
     var VideoRow = document.querySelector(".VideoRow");
     var VideoGrid = document.querySelector(".videoGrid");
+    var showLessDiv = document.querySelector(".showLessDiv");
+    var MoreVideo = document.querySelector('.MoreVideo')
     VideoRow.style.display = 'flex';
+    MoreVideo.style.display = 'flex';
     VideoGrid.style.display = 'none';
+    showLessDiv.style.display = 'none';
   //  alert("Load More Videos");
 }
